4f38784cbe01a01ef2122d5a0b93983377b2306b
Stipe-Kurir <skurir00@fesb.hr>
2023-01-23 21:05:41 +0100

    Header,Navbar design

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <div className="navContainer">
+            <span className="logo">BookingApp</span>
+            <div className="navItems">
+                <button className="navButton">Register</button>
+                <button className="navButton">Log in</button>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar';
+import "./home.css";
+import Header from "../../components/header/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Header/>
+    </div>
+  )
+}
+
+export default Home;
