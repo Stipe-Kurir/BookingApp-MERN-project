d576d6b97ea1f51d4e7d3f485d8eb96acba11950
Stipe-Kurir <skurir00@fesb.hr>
2023-01-25 21:44:44 +0100

    Search Bar Design

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,9 +10,24 @@ import {format} from "date-fns";
 
 
 
-const Header = () => {
+const Header = ({type}) => {
   
   const [openDate,setOpenDate]=useState(false);
+  const [openOptions,setOpenOptions]=useState(false);
+  const [options,setOptions]=useState({
+    adult:1,
+    children:0,
+    room:1,
+  });
+
+  const handleOption=(name,operation)=>{
+    setOptions((prev)=>{
+      return{
+      ...prev,
+      [name]: operation === "i" ? options[name] +1 : options[name]-1,
+    };
+  });
+  };
 
   const [date, setDate]=useState([
      {
@@ -24,7 +39,7 @@ const Header = () => {
 
   return (
     <div className="header">
-      <div className="headerContainer">
+      <div className={ type ==="list" ? "headerContainer listMode" : "headerContainer"}>
        <div className="headerList">
         <div className="headerListItem active">
             <FontAwesomeIcon icon={faBed}/>
@@ -48,7 +63,9 @@ const Header = () => {
         </div>
 
         </div>
-        <h1 className="headerTitle">A lifetime of discounts?It's genius.</h1>
+        { type !== "list" &&
+          <>
+          <h1 className="headerTitle">A lifetime of discounts? It's genius.</h1>
         <p className="headerDesc">Get rewarded for your travels – unlock instant savings of 10% or
               more with a free Lamabooking account </p>
         <button className="headerBtn"> Sign in / Register</button>
@@ -72,14 +89,56 @@ const Header = () => {
             className="date"
             />}
           </div>
+
           <div className="headerSearchItem">
             <FontAwesomeIcon icon={faPerson} className="headerIcon" />
-            <span className="headerSearchText">2 adults 2 children 1 room</span>
+            <span onClick={()=>setOpenOptions(!openOptions)} className="headerSearchText">{`${options.adult} adult · ${options.children} children · ${options.room} room `}</span>
+            {openOptions && 
+            <div className="options">
+
+              <div className="optionItem">
+                <span className="optionText">Adult</span>
+                <div className="optionCounter">
+                <button 
+                disabled={options.adult <= 1}
+                className="optionCounterButton" onClick={()=>handleOption("adult","d")}>-</button>
+                <span className="optionCounterNumber">{options.adult}</span>
+                <button className="optionCounterButton" onClick={()=>handleOption("adult","i")}>+</button>
+                </div>
+              </div>
+
+              <div className="optionItem">
+              <span className="optionText">Children</span>
+               <div className="optionCounter">     
+                <button 
+                disabled={options.children <= 0}
+                className="optionCounterButton" onClick={()=>handleOption("children","d")}>-</button>
+                <span className="optionCounterNumber">{options.children}</span>
+                <button className="optionCounterButton" onClick={()=>handleOption("children","i")}>+</button>
+               </div>
+              </div>
+
+              <div className="optionItem">
+              <span className="optionText">Room</span>
+               <div className="optionCounter">
+                <button 
+                disabled={options.room <= 1}
+                className="optionCounterButton" onClick={()=>handleOption("room","d")}>-</button>
+                <span className="optionCounterNumber">{options.room}</span>
+                <button className="optionCounterButton" onClick={()=>handleOption("room","i")}>+</button>
+               </div>
+              </div>
+
+            </div>
+}
+
           </div>
+
           <div className="headerSearchItem">
             <button className="headerBtn">Search</button>
           </div>
-        </div>
+        </div> 
+        </>}
       </div>
     </div>
   )
--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Header from '../../components/header/Header';
+import Navbar from '../../components/navbar/Navbar';
 
 const List = () => {
   return (
     <div>
-      Hotels
+      <Navbar/>
+      <Header type="list"/>
     </div>
   )
 }
