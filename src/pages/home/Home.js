6536dbb747865842662909504e6bb55c842a22a2
Stipe-Kurir <skurir00@fesb.hr>
2023-01-26 20:15:13 +0100

    homepage lists

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,12 +2,17 @@ import React from 'react'
 import Navbar from '../../components/navbar/Navbar';
 import "./home.css";
 import Header from "../../components/header/Header";
+import Featured from '../../components/featured/Featured';
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+      </div>
     </div>
   )
 }
