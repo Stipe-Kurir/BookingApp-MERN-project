403206b37cab74e4216b7a08e9a2bfc3a9b7b81a
Stipe-Kurir <skurir00@fesb.hr>
2023-01-27 21:16:38 +0100

    Homepage Lists

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,6 +3,9 @@ import Navbar from '../../components/navbar/Navbar';
 import "./home.css";
 import Header from "../../components/header/Header";
 import Featured from '../../components/featured/Featured';
+import PropertyList from '../../components/propertyList/PropertyList';
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
+
 
 const Home = () => {
   return (
@@ -11,7 +14,12 @@ const Home = () => {
       <Header/>
       <div className="homeContainer">
         <Featured />
+        <div className="razmak"> {/* doda kodradi izgleda*/}
         <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList/>
+        </div>
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties/>
       </div>
     </div>
   )
