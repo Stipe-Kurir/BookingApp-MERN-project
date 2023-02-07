2944b63de3c6d2aedcb7d1d6693a323114b6b3a2
Stipe-Kurir <skurir00@fesb.hr>
2023-02-07 21:14:03 +0100

    Creating API using Node.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+
 const app=express();
 dotenv.config(); //bitno za -env varijable
 
@@ -17,10 +22,21 @@ mongoose.connection.on("disconnected",()=>{
     console.log("mongoDB disconnected");
 });
 
+//middlewares
+
+app.use("/api/auth",authRoute);
+app.use("/api/user",usersRoute);
+app.use("/api/hotels",hotelsRoute);
+app.use("/api/rooms",roomsRoute);
+
+
+
+
+/*
 mongoose.connection.on("connected",()=>{
     console.log("mongoDB connected");
 });
-
+*/
 
 
 app.listen(8800, ()=>{
