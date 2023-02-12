99f6f3dcd184009d52adb24b53d23398a1d8ad4e
Stipe-Kurir <skurir00@fesb.hr>
2023-02-12 19:55:55 +0100

    How to fetch data using a custom hook

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 
 const app=express();
 dotenv.config(); //bitno za -env varijable
@@ -25,6 +25,7 @@ mongoose.connection.on("disconnected",()=>{
 });
 
 //middlewares
+app.use(cors());
 app.use(cookieParser());
 
 app.use(express.json());
