528e04c755e39d388635fbb8ec071aa26ca15436
Stipe-Kurir <skurir00@fesb.hr>
2023-02-13 16:15:46 +0100

    React Fetch Data from Node Express API

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 import "./navbar.css"
 
 const Navbar = () => {
   return (
     <div className="navbar">
         <div className="navContainer">
+          <Link to="/" style={{color:"inherit",textDecoration:"none"}}>
             <span className="logo">BookingApp</span>
+            </Link>
             <div className="navItems">
                 <button className="navButton">Register</button>
                 <button className="navButton">Log in</button>
